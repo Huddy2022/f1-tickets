6d29ad83d8e3c7eae5e4dadc2561e922ace18c0f
Huddy2022 <>
2023-09-04 19:17:25 +0000

    set debug to false and get ready for deployment, also edit my orders html to be more responsive on mobile

--- F1/settings.py
+++ F1/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['f1-tickets-e355687b93ca.herokuapp.com', 'localhost',
                  '8000-huddy2022-f1-tickets-gllzk9garf.us2.codeanyapp.com']
